301720c92feb17e1f6c487af6db7a1dea598dc58
fenclova <fenclova.karolina@gmail.com>
2018-04-26 15:26:26 +0200

    Description added

--- watch_dog.py
+++ watch_dog.py
@@ -1,10 +1,16 @@
+# ....................................................................................................
+# Name:         watch_dog.py
+# Author:       Karolina Fenclova
+# Description:  Script will call another script until a set "condition"
+#               To solve the problem with a prematurely terminated calculation.
 
 
 import subprocess
-print "ahoj"
+print "watch_dog.py started:"
 
 while True:
-    return_code = subprocess.call([r"C:\Python27\ArcGIS10.5\python.exe", r"C:\fenclova\diplomka\analyza\skripty\0_predvyber_uzemi.py"])
+    # Call 0_predvyber_uzemi.py until exit code is 777
+    return_code = subprocess.call([r"C:\Python27\ArcGIS10.5\python.exe", r"C:\fenclova\diplomka\analyza\git_diplomka\0_predvyber_uzemi.py"])
     print ("navratovy kod pythonu.")
     if return_code == 777:
         break
